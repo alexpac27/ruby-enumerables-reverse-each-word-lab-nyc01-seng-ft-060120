0616070464b24a05b1e9e0c8fe481cc3e129b921
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-28 18:56:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
-def reverse_each_word(string)
+def reverse_each_word("string")
 array = %w[string]
 reverse_array = array.collect{ |e| e.reverse}
 p reverse_array.join(" ")
 end
 
-#reverse_each_word(This is a string!)
+reverse_each_word("This is a string!"")
